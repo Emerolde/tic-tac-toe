6eb7853204f635c86787f0484c8f38ad0b49c33f
emerolde <emerolde1234@gmail.com>
2024-01-06 21:29:15 -0500

    work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
 import './App.css';
 
+import { Board } from "./components/Board"
+
 function App() {
+
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXplaying] = useState(true);
+  const [scores, setScores] = useState({ xScore:0, oScore:0 })
+  
+  const handleBoxClick = (boxIdx) => {
+   const updatedBoard = board.map((value, idx) => {
+    if(idx === boxIdx) {
+      return xPlaying === true ? "X" : "O";
+    } else {
+      return value;
+    }
+  
+   })
+
+   const winner = checkWinner(updatedBoard);
+
+   if(winner) { 
+    if(winner === "O"){
+      let {oScore} = scores;
+      oScore += 1
+      setScores({...scores, oScore})
+    } else {
+      if(winner === "O"){
+        let {xScore} = scores;
+        xScore += 1
+        setScores({...scores, xScore})
+    }
+   }
+
+   setBoard(updatedBoard);
+
+   setXplaying(!xPlaying);
+  }
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length; i++){
+      const [x,y,z] = WIN_CONDITIONS[i]
+
+      if(board[x] && board[x] === board[y] && board[y] === board[z]){
+        console.log(board[x])
+        return board[x]; 
+      }
+    }
+
+    console.log(scores)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board board ={board} onClick={handleBoxClick}/>
     </div>
   );
 }
